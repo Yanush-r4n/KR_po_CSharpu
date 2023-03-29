d859c06a8262fadea9fa14e57e0dcf046b50cba0
Yanush-r4n <yana.mur.04@gmail.com>
2023-03-29 03:46:19 +0300

    first solution to C# test

--- Program.cs
+++ Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+string[] ArrayOfWordsLessThen4Letters(string[] array)
+{
+    int count = 0;
+    int result = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        for (int j = 0; j < array[i].Length; j++)
+        {
+            count = 1;
+            if (j > 2) {count = 0; break;}
+        }
+
+        result += count;
+    }
+
+    string[] desiredArray = new string[result];
+
+    int k = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        for (int j = 0; j < array[i].Length; j++)
+        {
+            count = 1;
+            if (j > 2) {count = 0; break;}
+        }
+
+        if (count == 1) {desiredArray[k++] = array[i];}
+    }
+
+    return desiredArray;
+}
+
+string[] array = new string[] {"1447", "легенда", "кот", "7", "киш", "ахахахаха", "миш", "hero", "ice"};
+
+Console.WriteLine($"[{String.Join(", ", array)}]");
+Console.WriteLine($"[{String.Join(", ", ArrayOfWordsLessThen4Letters(array))}]");
